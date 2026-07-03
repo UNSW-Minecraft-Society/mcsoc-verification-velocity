c421c054b59f280922fb1593ba5c13fe049d09a0
Veveddo <veveddo@macbook>
2026-07-03 18:39:32 +1000

    implement command registration

--- src/main/java/com/mcsoc/verificationvelocity/VerificationPlugin.java
+++ src/main/java/com/mcsoc/verificationvelocity/VerificationPlugin.java
@@ -1,29 +1,41 @@
 package com.mcsoc.verificationvelocity;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.command.BrigadierCommand;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
+import com.mcsoc.verificationvelocity.CommandRegistration;
+
 
 @Plugin(
-    id = "mcsoc-verification-velocity", 
+    id = VerificationPlugin.MODID, 
     name = "MCSoc Verification Plugin - Velocity", 
     version = "0.0.1",
     url = "https://mc.unswminecraft.com", 
     description = "A velocity plugin to interface with UNSW Minecraft Society's Firebase verification system.", 
     authors = {"veveddo"}
 )
-class VelocityTest {
+public class VerificationPlugin {
+    
+    public static final String MODID = "mcsoc-verification";
 
     private final ProxyServer server;
     private final Logger logger;
 
     @Inject
-    public VelocityTest(ProxyServer server, Logger logger) {
+    public VerificationPlugin(ProxyServer server, Logger logger) {
         this.server = server;
         this.logger = logger;
 
         logger.info("Hello there! I made my first plugin with Velocity.");
+    }
+
+    @Subscribe
+    public void onProxyInitialize(ProxyInitializeEvent event) {
+        CommandRegistration.registerCommands(server, this);
     }
 }
