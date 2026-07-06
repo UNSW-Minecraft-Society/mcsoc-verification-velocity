b770b1eb0e5916a213580d0652097e2c90db940d
veveddo <68318644+Vedvod@users.noreply.github.com>
2026-07-07 02:12:55 +1000

    implement config reload command

--- src/main/kotlin/com/mcsoc/verificationvelocity/eventhandlers/CommandRegistration.kt
+++ src/main/kotlin/com/mcsoc/verificationvelocity/eventhandlers/CommandRegistration.kt
@@ -1,42 +1,48 @@
 package com.mcsoc.verificationvelocity.eventhandlers
 
 import com.mojang.brigadier.Command
-import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
-import com.mojang.brigadier.tree.LiteralCommandNode
 import com.velocitypowered.api.command.BrigadierCommand
-import com.velocitypowered.api.command.CommandManager
 import com.velocitypowered.api.command.CommandSource
 import com.velocitypowered.api.proxy.ProxyServer
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
 
 import com.mcsoc.verificationvelocity.VerificationPlugin
+import com.mcsoc.verificationvelocity.dataloader.PluginDataLoader
 
 
 object CommandRegistration {
-    @JvmStatic
-    fun registerCommands(plugin: VerificationPlugin, proxy: ProxyServer) {
-        val manager = proxy.commandManager
-        val debug_command = this.registerDebugCommand(proxy)
-        val meta = manager.metaBuilder(debug_command)
-            // .aliases()
-            .plugin(plugin)
-            .build()
-            
-        manager.register(meta, debug_command)
-    }
     
-    private fun registerDebugCommand(proxy: ProxyServer): BrigadierCommand {
+    private fun constructDebugCommand(proxy: ProxyServer): BrigadierCommand {
         val rootNode: LiteralArgumentBuilder<CommandSource> = BrigadierCommand.literalArgumentBuilder(VerificationPlugin.MODID)
         .then(BrigadierCommand.literalArgumentBuilder("debug")
         .executes{ctx ->
             val source = ctx.source
-            source.sendMessage{
-                Component.text("Hello World", NamedTextColor.AQUA)
-            }
+            
             return@executes Command.SINGLE_SUCCESS
         })
         return BrigadierCommand(rootNode)
+    }
+    
+    private fun constructReloadConfigCommand(proxy: ProxyServer): BrigadierCommand {
+        val rootNode: LiteralArgumentBuilder<CommandSource> = BrigadierCommand.literalArgumentBuilder(VerificationPlugin.MODID)
+        .then(BrigadierCommand.literalArgumentBuilder("reload")
+        .executes{ctx ->
+            val source = ctx.source
+            PluginDataLoader.loadDataFromFiles()
+            return@executes Command.SINGLE_SUCCESS
+        })
+        return BrigadierCommand(rootNode)
+    }
+    
+    @JvmStatic
+    fun registerCommands(plugin: VerificationPlugin, proxy: ProxyServer) {
+        val manager = proxy.commandManager
+        
+        val debug_command = this.constructDebugCommand(proxy)
+        val debug_meta = manager.metaBuilder(debug_command)
+            // .aliases()
+            .plugin(plugin)
+            .build()
+        manager.register(debug_meta, debug_command)
     }
 }
